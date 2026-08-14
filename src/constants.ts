f837f118c6c624e047d5d231024b0f521f1ba9f2
Aldin Hndr <aldinhalawa2023@gmail.com>
2026-08-14 18:28:56 +0700

    UPDATE LOGIN ADMIN dan GPIN

--- src/constants.ts
+++ src/constants.ts
@@ -21,15 +21,15 @@ export const GPIN_CHURCHES = [
 export type GpinChurchName = (typeof GPIN_CHURCHES)[number]
 
 export const GPIN_PASTOR_ANSWERS: Record<GpinChurchName, string[]> = {
-  'GPIN Eklesia Mesuji': ['Pdt. Samuel Toro'],
-  'GPIN Eben Haezer Natar': ['natar'],
-  'GPIN Bukit Zaitun Panjang': ['panjang'],
-  'GPIN Betlehem Sidomulyo': ['Pdt. Giono'],
+  'GPIN Eklesia Mesuji': ['Ev. Samuel Toro'],
+  'GPIN Eben Haezer Natar': ['Pdt. Untung', '[Pdt. Untung Basuki', 'Pak Basuki'],
+  'GPIN Bukit Zaitun Panjang': ['Pdt. Ricardono Lubis', 'Pdt. Rikardono Lubis', 'Pdt. Ricardono', 'Pdt. Ricardono'],
+  'GPIN Betlehem Sidomulyo': ['Pdt. Giono', 'Pak Giono'],
   'GPIN Filipi Way Halim': ['Pdt. Yakob', 'Pak Yakob'],
-  'GPIN Gloria Kotabaru Itera': ['itera', 'kotabaru'],
+  'GPIN Gloria Kotabaru Itera': ['Valen Gea', 'Tante Valen'],
   'GPIN Filadelfia Polresta': ['Pdt. Mardi Utomo', 'Pdt. Mardi', 'Pak Mardi'],
-  'GPIN Hosanna Langkapura': ['langkapura', 'hosanna'],
-  'GPIN Haleluya Bergen': ['bergen'],
-  'GPIN Elshadday Karanganyar': ['karanganyar', 'elshadday'],
+  'GPIN Hosanna Langkapura': ['Pdt. Ingati Zega', 'Pdt. Ingati', 'Pak Ingati'],
+  'GPIN Haleluya Bergen': ['Ev. Darwis', 'Om Darwis'],
+  'GPIN Elshadday Karanganyar': ['Ev. Yunus', 'Om Yunus'],
   'GPIN Perintisan Srimukti': ['srimukti'],
 }
